9204a39c8d529f9a98904eb009fcfd3823a70a9a
alenadmit <dmitalyona@yandex.ru>
2019-01-16 00:06:53 +0300

    added tasklist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './components/task/task.component';
+import { TasklistComponent } from './components/tasklist/tasklist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TasklistComponent
   ],
   imports: [
     BrowserModule,
